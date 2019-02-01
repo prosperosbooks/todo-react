fa63d5c511e056b739705b85234e0202f2bfc318
Emin <allen.emin@gmail.com>
2019-01-31 23:47:40 -0500

    first-commit

---
+++ src/components/Todos.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Todos = () => {
+  return (
+    <div>
+      <p>
+        <label>Fold the laundry</label>
+        <input type="checkbox" />
+      </p>
+
+      <p>
+        <label>Wash the dishes</label>
+        <input type="checkbox" />
+      </p>
+
+      <p>
+        <label>Walk the dags</label>
+        <input type="checkbox" />
+      </p>
+    </div>
+  );
+};
+
+export default Todos;
